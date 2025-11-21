70412c64296b76df02cd895a3be3d800434051bb
Dyuti Sharma <Dyuti.21983@stu.upes.ac.in>
2025-11-21 18:11:54 +0530

    Update Experiment 6.2.C

--- Experiment 6.2.C
+++ Experiment 6.2.C
@@ -1,1 +1,21 @@
-
+/* 4.3 Patterns: 11*/
+#include<stdio.h>
+int main()
+{
+ printf("DYUTI SHARMA\nSAP ID: 590021983\nCourse: BSc. CS.
+\nBatch: 1");
+ printf("\n-----------------\n");
+ printf("The pattern is:\n");
+ int i, l;
+ for(i=1; i<=6; i++)
+ {
+ int m=7-i;
+ for(l=1; l<i; l++)
+ {
+ printf("%d", m);
+ m++;
+ }
+ printf("\n");
+ }
+ return 0;
+ }
